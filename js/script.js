01abe487e2ec2eebea63e6dc9d971fdfc6aa7ee3
Lexis von Seggern <lexvon2015@gmail.com>
2017-08-29 15:24:31 -0500

    some styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $('.mobile-nav-btn').click(function(e){
-		$('.mobile-nav').toggle();
+		$('.mobile-nav').slideToggle();
 		// this prevents the browser from doing the default link action
 		e.preventDefault();
 	});
+
 var $window = $(window);
 	  $window.on('resize', function (){
         if ($window.width() > 800)
